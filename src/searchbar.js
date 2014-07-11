a040fcf2905d2cafa35f56235fd8bb5051169f21
dragonshade <rxxxie@gmail.com>
2014-07-11 14:17:35 -0700

    refactored

--- src/searchbar.js
+++ src/searchbar.js
@@ -1,167 +1,172 @@
-document.addEventListener('DOMContentLoaded', init, false);
+document.addEventListener('DOMContentLoaded', function(){ var ts = new TabSearch();}, false);
 
-var TabSearch = document.TabSearch = {}
+(function(root){
+    var TabSearch = root.TabSearch = function (){
 
+        //Initializing UI and keyHandling
+        this.searchbar = document.getElementById('search');
+        this.searchbar.focus();
+        this.searchbar.addEventListener('keyup', this.keyInputHandler.bind(this));
+        this.resultList = document.getElementById('result');
 
-function init(){
+        //Internal Variables
+        this.activeElement = null;
 
-  chrome.tabs.query({}, function(tabs){
-    var result = document.getElementById('result');
-    TabSearch.tabs = tabs;
-  })
-
-  var searchbar = document.getElementById('search');
-  searchbar.focus();
-  searchbar.addEventListener('keyup', keyInputHandler)
-}
-
-function keyInputHandler(event){
-  if(!event.altKey && !event.ctrlKey){
-    if(event.keyIdentifier == "Enter"){
-      var tabId = parseInt(TabSearch.activeElement.dataset.id);
-      chrome.tabs.get(tabId, function(tab){
-        chrome.windows.update(tab.windowId, {focused: true}, function(window){
+        //Caching tabs on startup
+        var that = this;
+        chrome.tabs.query({}, function(tabs){
+            that.tabs = tabs;
         })
-      })
-      chrome.tabs.update(
-        tabId,
-        {selected: true}
-      );         
     }
-    else if(event.keyIdentifier == "Up"){
-      if(TabSearch.activeElement.previousSibling){
-        removeClass(TabSearch.activeElement, "active");
-        TabSearch.activeElement = TabSearch.activeElement.previousSibling;
-        addClass(TabSearch.activeElement, "active");
-      }
-    }
-    else if(event.keyIdentifier == "Down"){
-      if(TabSearch.activeElement.nextSibling){
-        removeClass(TabSearch.activeElement, "active");
-        TabSearch.activeElement = TabSearch.activeElement.nextSibling;
-        addClass(TabSearch.activeElement, "active");
-      }
-    }
-    else{
-      filter(event);
-    }
-  }
-}
 
-function addClass(domElement, classname){
-  var case1 = new RegExp(classname + " ");
-  var case2 = new RegExp(" " + classname);
-  var case3 = new RegExp("^" + classname + "$");
-  if(domElement.className.length == 0){
-    domElement.className += classname;
-  }
-  else if(
-    domElement.className.search(case1) < 0 &&
-    domElement.className.search(case2) < 0 &&
-    domElement.className.search(case3) < 0 ){
-
-    domElement.className += (" " + classname);
-  }
-}
-
-function hasClass(domElement, classname){
-  var case1 = new RegExp(classname + " ");
-  var case2 = new RegExp(" " + classname);
-  var case3 = new RegExp("^" + classname + "$");
-  if(
-    domElement.className.search(case1) > -1 ||
-    domElement.className.search(case2) > -1 ||
-    domElement.className.search(case3) > -1 ){
-    return true;
-  }else{
-    return false;
-  }
-}
-
-function removeClass(domElement, classname){
-  var case1 = new RegExp(classname + " ");
-  var case2 = new RegExp(" " + classname);
-  var case3 = new RegExp("^" + classname + "$");
-  if(domElement.className.search(case1) > -1){
-    domElement.className = domElement.className.replace(case1, "");
-  }
-  else if(domElement.className.search(case2) > -1){
-    domElement.className = domElement.className.replace(case2, "");
-  }
-  else if(domElement.className.search(case3) > -1){
-    domElement.className = domElement.className.replace(case3, "");
-  }
-}
-
-function filter(event){
-  var searchStr = event.target.value
-
-  var first = searchStr.split(' ');
-  var firstPass = [];
-  for(var i = 0; i < first.length; i++){
-    firstPass.push(new RegExp(first[i],'i'));
-  }
-
-  var secondPass = new RegExp(searchStr.replace(/\s*/,"").split('').join('.*'), 'i');
-
-  var result = [];
-  var checked = {};
-
-  var matched = true;
-
-  if(searchStr.length > 0){
-    for(var i = 0; i < TabSearch.tabs.length; i++){
-      for(var j = 0; j < firstPass.length; j++){
-        if(TabSearch.tabs[i].url.search(firstPass[j]) < 0 && 
-          TabSearch.tabs[i].title.search(firstPass[j]) < 0 ){
-          matched = false;
+    TabSearch.prototype.keyInputHandler = function(event) {
+        if(!event.altKey && !event.ctrlKey){
+            if(event.keyIdentifier == "Enter" && this.activeElement){
+                var tabId = parseInt(this.activeElement.dataset.id);
+                chrome.tabs.get(tabId, function(tab){
+                    chrome.windows.update(tab.windowId, {focused: true})
+                })
+                chrome.tabs.update(
+                    tabId,
+                    {selected: true}
+                );         
+            }
+            else if(event.keyIdentifier == "Up"){
+                if(this.activeElement.previousSibling){
+                    removeClass(this.activeElement, "active");
+                    this.activeElement = this.activeElement.previousSibling;
+                    addClass(this.activeElement, "active");
+                }
+            }
+            else if(event.keyIdentifier == "Down"){
+                if(this.activeElement.nextSibling){
+                    removeClass(this.activeElement, "active");
+                    this.activeElement = this.activeElement.nextSibling;
+                    addClass(this.activeElement, "active");
+                }
+            }
+            else{
+                this.filter(event);
+            }
         }
-      }
-      if(matched){
-        result.push(TabSearch.tabs[i])
-        checked[TabSearch.tabs[i].id] = true;
-      }
-      matched = true;
-      if(result.length > 4){
-        break;
-      }
     }
 
-    if(result.length < 6){
-      for(var i = 0; i < TabSearch.tabs.length; i++){
-        if(!checked[TabSearch.tabs[i].id]){
-          if(TabSearch.tabs[i].url.search(secondPass) > -1 || 
-            TabSearch.tabs[i].title.search(secondPass) > -1 ){
-            result.push(TabSearch.tabs[i]);
+    TabSearch.prototype.filter = function(event) {
+        var searchStr = event.target.value
+        var first = searchStr.split(' ');
+        var firstPass = [];
+        var secondPass = new RegExp(searchStr.replace(/\s*/,"").split('').join('.*'), 'i');
+        var result = [];
+        var checked = {};
+        var matched = true;
+
+        for(var i = 0; i < first.length; i++){
+            firstPass.push(new RegExp(first[i],'i'));
+        }
+
+        if(searchStr.length > 0){
+          for(var i = 0; i < this.tabs.length; i++){
+              for(var j = 0; j < firstPass.length; j++){
+                  if(this.tabs[i].url.search(firstPass[j]) < 0 && 
+                      this.tabs[i].title.search(firstPass[j]) < 0 ){
+                      matched = false;
+                  }
+              }
+              if(matched){
+                  result.push(this.tabs[i])
+                  checked[this.tabs[i].id] = true;
+              }
+              matched = true;
+              if(result.length > 4){
+                  break;
+              }
+          }
+
+          if(result.length < 6){
+              for(var i = 0; i < this.tabs.length; i++){
+                  if(!checked[this.tabs[i].id]){
+                      if(this.tabs[i].url.search(secondPass) > -1 || 
+                        this.tabs[i].title.search(secondPass) > -1 ){
+                          result.push(this.tabs[i]);
+                      }
+                  }
+                  if(result.length > 4){
+                      break;
+                  }
+              }
           }
         }
-        if(result.length > 4){
-          break;
+      
+        this.resultList.innerHTML ="";
+        for(var i = 0; i < result.length; i++){
+          this.appendResult(result[i]);
         }
-      }
+        if(searchStr.length > 0){
+          this.activeElement = this.resultList.children[0];
+          addClass(this.resultList.children[0], "active");
+        }
     }
-  }
 
-  var resultList = document.getElementById('result');
-  resultList.innerHTML ="";
-  for(var i = 0; i < result.length; i++){
-    appendResult(result[i]);
-  }
-  if(searchStr.length > 0){
-    TabSearch.activeElement = resultList.children[0];
-    addClass(resultList.children[0], "active");
-  }
-}
+    TabSearch.prototype.appendResult = function(tab) {
+        var tabEntry = document.createElement('li');
+        var title = document.createElement('h4');
+        var url = document.createElement('p');
+        title.innerHTML = tab.title;
+        url.innerHTML = tab.url;
+        tabEntry.appendChild(title);
+        tabEntry.appendChild(url);
+        tabEntry.dataset.id = tab.id;
+        this.resultList.appendChild(tabEntry);
+    };
 
-function appendResult(tab){
-  var tabEntry = document.createElement('li');
-  var title = document.createElement('h4');
-  var url = document.createElement('p');
-  title.innerHTML = tab.title;
-  url.innerHTML = tab.url;
-  tabEntry.appendChild(title);
-  tabEntry.appendChild(url);
-  tabEntry.dataset.id = tab.id;
-  result.appendChild(tabEntry);
-}
+
+    //Helper methods
+    //Simple Version of addClass, removeClass, isClass implementation
+    function addClass(domElement, classname) {
+        var case1 = new RegExp(classname + " ");
+        var case2 = new RegExp(" " + classname);
+        var case3 = new RegExp("^" + classname + "$");
+        if(domElement.className.length == 0) {
+            domElement.className += classname;
+        }
+        else if(
+            domElement.className.search(case1) < 0 &&
+            domElement.className.search(case2) < 0 &&
+            domElement.className.search(case3) < 0 ) {
+
+            domElement.className += (" " + classname);
+        }
+    }
+
+    function hasClass(domElement, classname) {
+        var case1 = new RegExp(classname + " ");
+        var case2 = new RegExp(" " + classname);
+        var case3 = new RegExp("^" + classname + "$");
+        if(
+            domElement.className.search(case1) > -1 ||
+            domElement.className.search(case2) > -1 ||
+            domElement.className.search(case3) > -1 ) {
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function removeClass(domElement, classname) {
+        var case1 = new RegExp(classname + " ");
+        var case2 = new RegExp(" " + classname);
+        var case3 = new RegExp("^" + classname + "$");
+        if(domElement.className.search(case1) > -1) {
+            domElement.className = domElement.className.replace(case1, "");
+        }
+        else if(domElement.className.search(case2) > -1) {
+            domElement.className = domElement.className.replace(case2, "");
+        }
+        else if(domElement.className.search(case3) > -1) {
+            domElement.className = domElement.className.replace(case3, "");
+        }
+    }
+}(window));
+
 
